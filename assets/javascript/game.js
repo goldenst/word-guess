003bd00f30db21b63133542084aa7b318903abe6
MSI\jeff <hatcher28@me.com>
2018-12-29 00:45:14 -0800

    worked on logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,23 @@
 // game variables 
-var words = [ "Willie", "garth", "Toby", "Alabama", "SugarLand"];
+var words = ["willie", "garth", "toby", "alabama", "sugarland"];
 var losses = 0;
 var wins = 0;
 var guessesLeft = 12;
 var gussedLetters = [];
 var userGuess = " ";
 var underScore = [];
+var correctLetter =[];
+var wrongLetter = [];
 
-function newWord () {
-  
+function newWord() {
+
 }
-
 // clear array function
-function empty () {
+function empty() {
   gussedLetters = [];
 }
-
 // get word from array
-var getWord = words[Math.floor(Math.random () * words.length)];
+var getWord = words[Math.floor(Math.random() * words.length)];
 // get underscores for gamegf
 var getUnder = function () {
   for (i = 0; i < getWord.length; i++) {
@@ -27,53 +27,63 @@ var getUnder = function () {
 }
 console.log(getUnder());
 
+
 // Get user input
-document.onkeyup = function () {
+document.onkeyup = function (event) {
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
   // push user guess to aray
   gussedLetters.push(userGuess);
-  // decrese guesses left
-  guessesLeft--
   // reset guess counter
-  if (guessesLeft === 0){
+  if (guessesLeft === 0) {
     guessesLeft = 12;
     // increase losses
-    losses ++
+    losses++
     // call clear out array
     empty()
+    // call new word
     newWord()
   }
   
-  if( gussedLetters == getWord ) {
+  if (getWord.indexOf(userGuess) > -1){
+    correctLetter.push(userGuess);
+    for (var i = 0; i < getWord.length; i++) {
+      if(getWord[i] === userGuess) {
+        underScore[i] = userGuess;
+        console.log(underScore);
+      }
+    }
+  } else {
+    guessesLeft--
+    wrongLetter.push(userGuess)
+  }
+
+  // if user wins game
+  if (underScore == getWord) {
     alert('winner')
-    wins ++;
+    wins++;
     guessesLeft = 12;
     empty()
     newWord()
   }
-   
 
-
-
-
-
-
-  var html = "<p>Guess The Prase Above</p>" +
-  "<p>Word in aray: " + words + "</p>" +
-  "<p>Word to guess: " +  underScore + "</p>" +
-  "<p>Wins: " + wins + "</p>" +
-  "<p>Loses: " + losses + "</p>" +
-  "<p>Guesses Left: " + guessesLeft + "</p>" +
-  "<p>Letters Gussed: " + gussedLetters+ "</p>" ;
+  var html = "<p>Guess The Prase Below</p>" +
+    "<p>Word to guess: " + underScore + "</p>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Loses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Letters Gussed: " + gussedLetters + "</p>";
 
   document.querySelector("#word").innerHTML = html;
- // logs to screen for testing 
+  // logs to screen for testing 
+
+
   
- 
-  console.log(gussedLetters);
-  console.log(getWord)
+  console.log("Guessed letters: " + gussedLetters);
+  console.log("Correect letter: " + correctLetter);
+  console.log("Wrong letter: " + wrongLetter);
+  console.log(getWord);
   console.log("User Guess: " + userGuess);
-  console.log("guesses Left: " + guessesLeft + " Wins: " + wins +" Losses: " + losses)
+  console.log("guesses Left: " + guessesLeft + " Wins: " + wins + " Losses: " + losses)
 }
 
 //console.log(gussesLetters);
